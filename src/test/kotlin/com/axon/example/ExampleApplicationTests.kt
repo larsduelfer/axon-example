fe8f0dd5d5866412987f6aa66370c972d0314fff
Lars Duelfer <lars.duelfer@novatec-gmbh.de>
2021-07-02 10:13:06 +0000

    Initial project setup with axon framework dependency

---
+++ src/test/kotlin/com/axon/example/ExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.axon.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
